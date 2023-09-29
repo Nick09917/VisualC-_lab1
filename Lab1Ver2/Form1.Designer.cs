b3b645546b117d2927de7e833cabb834187475f5
nlck0 <Kolya123265@gmail.com>
2023-09-29 14:36:07 +0600

    Добавьте файлы проекта.

---
+++ Lab1Ver2/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Lab1Ver2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(33, 289);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Кнопка 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(191, 152);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(429, 108);
+            textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(146, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(509, 82);
+            label1.TabIndex = 2;
+            label1.Text = resources.GetString("label1.Text");
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(236, 289);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 50);
+            button2.TabIndex = 0;
+            button2.Text = "Кнопка 2 ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(443, 289);
+            button3.Name = "button3";
+            button3.Size = new Size(140, 50);
+            button3.TabIndex = 0;
+            button3.Text = "Кнопка 3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(625, 289);
+            button4.Name = "button4";
+            button4.Size = new Size(140, 50);
+            button4.TabIndex = 0;
+            button4.Text = "Кнопка 4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(191, 129);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Текст вводить тут";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 421);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 20);
+            label3.TabIndex = 4;
+            label3.Text = "made by Nlck0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Лаб работа № 1 Осипов Н.М";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox2;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Lab1Ver2/Form1.cs
@@ -0,0 +1,68 @@
+namespace Lab1Ver2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.AntiqueWhite;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                Console.WriteLine("pass");
+                MessageBox.Show("Ничего не введено", "Попался!");
+            }
+            else
+                button1.Text = textBox2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                Console.WriteLine("pass");
+                MessageBox.Show("Ничего не введено", "Попался!");
+            }
+            else
+                button2.Text = textBox2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                Console.WriteLine("pass");
+                MessageBox.Show("Ничего не введено", "Попался!");
+            }
+            else
+            {
+
+                button3.Text = textBox2.Text;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                Console.WriteLine("pass");
+                MessageBox.Show("Ничего не введено","Попался!");
+            }
+            else
+                button4.Text = textBox2.Text;
+        }
+    }
+}
